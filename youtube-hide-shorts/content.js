b6eafe0309f93acc65eb0dc908a1e6cd5e670d06
Mufeez Amjad <19630228+mufeez-amjad@users.noreply.github.com>
2022-11-03 17:19:31 -0400

    Cleanup content.js

--- youtube-hide-shorts/content.js
+++ youtube-hide-shorts/content.js
@@ -2,30 +2,18 @@ addMutationObserver();
 
 function addMutationObserver() {
     const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-          mutation.target.querySelectorAll("#items").forEach(handleElement);
-        });
+      mutations.forEach(function (mutation) {
+        mutation.target.querySelectorAll("#items").forEach(handleElement);
       });
-      observer.observe(document.body, { subtree: true, childList: true });      
+    });
+    observer.observe(document.body, { subtree: true, childList: true });      
 }
 
 function handleElement(e) {
   e.childNodes.forEach((child) => {
-    if (child) {
-      const anchor = child.querySelectorAll("#thumbnail");
-
-      if (anchor) {
-        let del = false;
-        anchor.forEach((a) => {
-          if (a.getAttribute('href').startsWith("/shorts")) {
-            del = true;
-          }
-        });
-
-        if (del) {
-          child.remove();
-        }
-      }
+    let anchor = child.querySelector("#thumbnail");
+    if (anchor && (anchor.getAttribute('href') || '').startsWith("/shorts")) {
+        child.remove();
     }
   });
 }
